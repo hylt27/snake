114aeae00d3517e65d82fa5a24ebc96351a04b42
Henrique Yukio <hylt27@gmail.com>
2023-01-25 00:09:31 -0300

    added logic for snake movement using the sprites

--- setup.py
+++ setup.py
@@ -2,15 +2,16 @@ import pygame, sys
 from pygame.math import Vector2
 import random
 
-class Fruit:
+class Rat:
     def __init__(self):
         self.randomize()
 
 
-    def draw_fruit(self):
-        # create a rect for the fruit and draw it
-        fruit_rect = pygame.Rect(self.position.x*cell_size, self.position.y*cell_size, cell_size, cell_size)
-        pygame.draw.rect(screen, 'red', fruit_rect)
+    def draw_rat(self):
+        # create a rect for the rat and draw it
+        rat_rect = pygame.Rect(self.position.x*cell_size, self.position.y*cell_size, cell_size, cell_size)
+        screen.blit(rat, rat_rect)
+
 
     def randomize(self):
         # creates a random position vector
@@ -25,12 +26,62 @@ class Snake:
         self.direction = Vector2(1,0)
         self.new_block = False
 
+        # sprites for snake body
+        self.head_up = pygame.image.load('Graphics/head_up.png').convert_alpha()
+        self.head_down = pygame.image.load('Graphics/head_down.png').convert_alpha()
+        self.head_left = pygame.image.load('Graphics/head_left.png').convert_alpha()
+        self.head_right = pygame.image.load('Graphics/head_right.png').convert_alpha()
+
+        self.tail_up = pygame.image.load('Graphics/tail_up.png').convert_alpha()
+        self.tail_down = pygame.image.load('Graphics/tail_down.png').convert_alpha()
+        self.tail_left = pygame.image.load('Graphics/tail_left.png').convert_alpha()
+        self.tail_right = pygame.image.load('Graphics/tail_right.png').convert_alpha()
+
+        self.body_vertical = pygame.image.load('Graphics/body_vertical.png').convert_alpha()
+        self.body_horizontal = pygame.image.load('Graphics/body_horizontal.png').convert_alpha()
+
+        self.body_tr = pygame.image.load('Graphics/body_tr.png').convert_alpha()
+        self.body_tl = pygame.image.load('Graphics/body_tl.png').convert_alpha()
+        self.body_br = pygame.image.load('Graphics/body_br.png').convert_alpha()
+        self.body_bl = pygame.image.load('Graphics/body_bl.png').convert_alpha()
+
     def draw_snake(self):
-        for block in self.body:
+        for index, block in enumerate(self.body):
             x_pos = block.x*cell_size
             y_pos = block.y*cell_size
             snake_rect = pygame.Rect(x_pos, y_pos, cell_size, cell_size)
-            pygame.draw.rect(screen, (104, 71, 141), snake_rect)
+
+
+            if index == 0:
+                self.update_head_graphics(snake_rect)
+            elif index == len(self.body) - 1:
+                self.update_tail_graphics(snake_rect)
+            else:
+                pygame.draw.rect(screen, (104, 71, 141), snake_rect)
+
+    def update_head_graphics(self, rect):
+        head_direction = self.body[0] - self.body[1]
+
+        if head_direction == Vector2(1,0):
+            screen.blit(self.head_right, rect)
+        elif head_direction == Vector2(-1,0):
+            screen.blit(self.head_left, rect)
+        elif head_direction == Vector2(0,1):
+            screen.blit(self.head_down, rect)
+        elif head_direction == Vector2(0,-1):
+            screen.blit(self.head_up, rect)
+
+    def update_tail_graphics(self, rect):
+        tail_direction = self.body[-2] - self.body[-1]
+
+        if tail_direction == Vector2(1,0):
+            screen.blit(self.tail_left, rect)
+        elif tail_direction == Vector2(-1,0):
+            screen.blit(self.tail_right, rect)
+        elif tail_direction == Vector2(0,1):
+            screen.blit(self.tail_up, rect)
+        elif tail_direction == Vector2(0,-1):
+            screen.blit(self.tail_down, rect)
 
     def move_snake(self):
         if self.new_block == True:
@@ -49,7 +100,7 @@ class Snake:
 class Main:
     def __init__(self):
         self.snake = Snake()
-        self.fruit = Fruit()
+        self.rat = Rat()
 
     def update(self):
         self.snake.move_snake()
@@ -57,13 +108,13 @@ class Main:
         self.check_fail()
 
     def draw_elements(self):
-        self.fruit.draw_fruit()
+        self.rat.draw_rat()
         self.snake.draw_snake()
 
     def check_collision(self):
-        # checks if the snake ate the fruit and then add a new block to the body
-        if self.fruit.position == self.snake.body[0]:
-            self.fruit.randomize()
+        # checks if the snake ate the rat and then add a new block to the body
+        if self.rat.position == self.snake.body[0]:
+            self.rat.randomize()
             self.snake.add_block()
 
     def check_fail(self):
@@ -88,9 +139,10 @@ class Main:
 # general settings
 pygame.init()
 cell_size = 40
-cell_number = 20
+cell_number = 15
 screen = pygame.display.set_mode((cell_size*cell_number, cell_size*cell_number))
 clock = pygame.time.Clock()
+rat = pygame.image.load('Graphics/rat.png').convert_alpha()
 
 
 surface = pygame.Surface((100, 200))
